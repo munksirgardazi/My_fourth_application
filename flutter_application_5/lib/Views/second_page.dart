a0ec35bd0e9dd87cd17bb1c03170624efa64e381
samikhafeez <samikhafeezkhan@gmail.com>
2022-09-09 17:24:34 +0500

    pata nahi

--- flutter_application_5/lib/Views/second_page.dart
+++ flutter_application_5/lib/Views/second_page.dart
@@ -1,19 +1,70 @@
 import 'package:flutter/material.dart';
 
 class SecondPage extends StatelessWidget {
-  String? name;
-  SecondPage({
-    this.name,
-    Key? key,
-  }) : super(key: key);
-
+  const SecondPage(
+      {Key? key,
+      required this.name,
+      required this.user,
+      required this.adrees,
+      required this.reason,
+      required this.password,
+      required this.confirmpassword})
+      : super(key: key);
+  final String name;
+  final String user;
+  final String adrees;
+  final String reason;
+  final String password;
+  final String confirmpassword;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(),
-      body: Center(
-        child: Container(
-          child: Text(name!),
+      body: Container(
+        color: Color.fromARGB(96, 2, 2, 2),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                Text('Usename'),
+                Container(
+                    height: 100,
+                    width: 400,
+                    decoration: BoxDecoration(
+                      color: Colors.amber,
+                    ),
+                    child: Text(name)),
+              ],
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                Text('Password'),
+                Container(
+                    height: 60,
+                    width: 200,
+                    decoration: BoxDecoration(
+                      color: Colors.amber,
+                    ),
+                    child: Text(name)),
+              ],
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Text('Reason to join'),
+                Container(
+                    height: 40,
+                    width: 100,
+                    decoration: BoxDecoration(
+                      color: Colors.amber,
+                    ),
+                    child: Text(name)),
+              ],
+            ),
+          ],
         ),
       ),
     );
--- flutter_application_5/lib/main.dart
+++ flutter_application_5/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_application_5/Views/new%20_project.dart';
 
 import 'Views/home_page.dart';
 
@@ -13,7 +14,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       theme: ThemeData(),
-      home: HomePage(),
+      home: NewProject(),
     );
   }
 }
